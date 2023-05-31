228458b30abe298c9b811e2ac31824906fd259e1
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-05-31 00:09:45 +0000

    added website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Charlotte Jhu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Charlotte Jhu
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+
+window.onload = function () {
+  // this calculates the volume of a sphere
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const radius = params.get('radius')
+
+  // process
+  const volume = (4 / 3) * Math.PI * radius ** 3
+  const dimensions = 'Radius (r) = ' + radius + ' units'
+
+  // output
+document.getElementById('radius').innerHTML = dimensions
+document.getElementById('volume').innerHTML = 'Volume (V) = ' + volume + ' units³'
+}
